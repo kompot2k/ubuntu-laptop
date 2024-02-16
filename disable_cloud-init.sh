d70189a926f2e9223a0d179f0ba38122bd5c50f1
Yurii Lapko <kompot2k@gmail.com>
2024-02-16 21:11:28 +0200

    Update disable_cloud-init.sh

--- disable_cloud-init.sh
+++ disable_cloud-init.sh
@@ -3,7 +3,7 @@
 touch /etc/cloud/cloud-init.disabled
 
 # uncheck everything except "None"
-sudo dpkg-reconfigure cloud-init
-apt-get purge cloud-init
+dpkg-reconfigure cloud-init
+apt purge cloud-init
 rm -rf /etc/cloud/ && rm -rf /var/lib/cloud/
 reboot
